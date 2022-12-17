3831dbadbce2460134b2e1182b3f2ca09ed9dbe6
ailqkson <120587546+ailqkson@users.noreply.github.com>
2022-12-17 22:44:29 +0200

    Delete fitblogapp directory

--- fitblogapp/fitblogapp/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from fitblogapp.users.views import UserRegister, UserEdit
-
-urlpatterns = [
-    path('register/', UserRegister.as_view(), name='register'),
-    path('edit_profile/', UserEdit.as_view(), name='edit-profile'),
-    ]
